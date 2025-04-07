907b9e3f32d68895301a865541c4e1349f4b09fe
vyppN <chwrtokuma@gmail.com>
2025-04-07 22:45:10 +0700

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 
 // Create a new Express application instance
 const app = express();
 
+// allow cross-origin requests from all origins
+app.use(cors());
+
+
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send('Hello World from Express.js!');
+});
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the API!' });
 });
 
 // Start the Express server
